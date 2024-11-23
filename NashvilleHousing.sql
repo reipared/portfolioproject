195b0844e7769d3cd4f66753c2d3871625085d78
unicornhimbo <reinaldopared@gmail.com>
2024-11-23 10:35:06 -0400

    Update NashvilleHousing.sql

--- NashvilleHousing.sql
+++ NashvilleHousing.sql
@@ -19,5 +19,22 @@ SET SaleDateConverted = CONVERT(Date,SaleDate)
 
 -- Populate Property Address data
 
-SELECT PropertyAddress
+SELECT *
 FROM PortfolioProject..NashvilleHousing
+-- WHERE PropertyAddress is null
+ORDER BY ParcelID
+
+SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject..NashvilleHousing a
+JOIN PortfolioProject..NashvilleHousing b
+    ON a.ParcelID = b.ParcelID
+    AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+UPDATE a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject..NashvilleHousing a
+JOIN PortfolioProject..NashvilleHousing b
+    ON a.ParcelID = b.ParcelID
+    AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
