aabb514ba4058cece72d1d05b2bd87aaea12f1ef
unicornhimbo <reinaldopared@gmail.com>
2024-12-01 12:04:03 -0400

    Update

--- Python/Automatic_File_Sorter_in_File_Explorer.py
+++ Python/Automatic_File_Sorter_in_File_Explorer.py
@@ -4,7 +4,7 @@ import os, shutil
 
 path = r"C:\Users\Reina\github\portfolioproject\Python\Data/"
 
-os.listdir(path)
+file_names =os.listdir(path)
 
 folder_names = ['xlsx files', 'image files', 'text files']
 
@@ -12,3 +12,14 @@ for loop in range(0,3):
     if not os.path.exists(path + folder_names[loop]):
         print(path + folder_names[loop])
         os.makedirs((path + folder_names[loop]))
+
+for file in file_names:
+    if ".xlsx" in file and not os.path.exists(path + "xlsx files/" + file):
+        shutil.move(path + file, path + "xlsx files/" + file)
+    elif ".jpg" in file and not os.path.exists(path + "image files/" + file):
+        shutil.move(path + file, path + "image files/" + file)
+    if ".txt" in file and not os.path.exists(path + "text files/" + file):
+        shutil.move(path + file, path + "text files/" + file)
+    else:
+        pass
+print("Done!")
